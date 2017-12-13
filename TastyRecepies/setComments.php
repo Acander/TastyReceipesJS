e7e64b30c160f84ba4487bf981ad11e45383ee6e
Acander <adrianz@kth.se>
2017-12-13 20:54:15 +0100

    Add files via upload

---
+++ TastyRecepies/setComments.php
@@ -0,0 +1,47 @@
+<?php 
+	require 'resources/fragments/init.php';
+	
+	use Functionality\Controller\Controller;
+	
+	use Functionality\Util\InputFiltering;
+	
+		echo $_POST['submit'];
+	
+		if(!isset($_POST['submit'])){
+			return;
+		}
+		
+		//InputFiltering:
+		
+			if(InputFiltering::commentInputFiltering($_POST["uid"])){
+				header("Location: ../TastyRecepies/index.php");
+				exit();
+			}
+			else if(InputFiltering::commentInputFiltering($_POST["date"])){
+				header("Location: ../TastyRecepies/index.php");
+				exit();
+			}
+			else if(InputFiltering::commentInputFiltering($_POST["message"])){
+				header("Location: ../TastyRecepies/index.php");
+				exit();
+			}
+			else if(InputFiltering::commentInputFiltering($_POST["food"])){
+				header("Location: ../TastyRecepies/index.php");
+				exit();
+			}
+			
+		$uid = $_POST['uid'];
+		$date = $_POST['date'];
+		$message = $_POST['message'];
+		$food = $_POST['food'];
+		
+		$contr = Controller::getController();
+		$contr->addANewComment($uid, $message, $date, $food);
+		
+		header("Location: ../TastyRecepies/resources/views/$food.php");
+		exit();
+		
+		
+
+	
+?>
